2f55f849d865ae497ed85231c0dc4cb2872419b2
Andreatito <andreasosa@gmail.com>
2023-04-19 23:43:51 +0100

    modindex

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,6 +26,14 @@ router.use('/crud',require('./crud'));
 router.use('/user', require('./users'));
 router.use('/atributos', require('./atributos'));
         
-
+ 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+  
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.hbs'))
+    });
+    
+  }
 
 module.exports = router;
